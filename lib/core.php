c105561779acd0f329b3b030ade36b921c318b09
Mark Harding <mark@marksmusiconline.co.uk>
2012-03-02 18:48:49 +0000

    fixed bug

--- lib/core.php
+++ lib/core.php
@@ -82,12 +82,21 @@ expose_function('site.river_feed',
 				false,
 				false);
 				
-function site_get_entities($type, $subtype,$container_guid,$owner_guid , $limit, $offset){
+function site_get_entities($type, $subtype,$container_guid,$owner_guid, $owner_name , $limit, $offset){
 	
+	if($owner_name){
+		$user = get_user_by_username($owner_name);
+	
+		$owner_guid = $user->guid;
+	}
+	if($container_guid == 0 || $owner_guid == 0){
+		$container_guid = ELGG_ENTITIES_ANY_VALUE;
+		$owner_guid = ELGG_ENTITIES_ANY_VALUE;
+	}
 	$options = array(	'type' => $type, 
 						'subtype' => $subtype,
-						'owner_guid' => $owner_guid,
 						'container_guid' => $container_guid,
+						'owner_guid' => $owner_guid,
 						'limit' => $limit,
 						'offset' => $offset
 						);
@@ -100,11 +109,11 @@ expose_function('site.get_entities',
 				"site_get_entities",
 				array(	'type' => array('type' => 'string', 'required' => false),
 						'subtype' => array('type' => 'string', 'required' => false),
-						'container_guid' => array('type' => 'int', 'required' => false,  'default' =>0),
-
-						'owner_guid' => array('type' => 'int', 'required' => false),
-						'limit' => array('type' => 'int','required' => false,),
-						'offset' => array('type' => 'int', 'required' => false,)),
+						'container_guid' => array('type' => 'int', 'required' => false,'default' => 0),
+						'owner_guid' => array('type' => 'int', 'required' => false,'default' => 0),
+						'owner_name' => array('type' => 'string', 'required' => false, 'default' => 'all'),
+						'limit' => array('type' => 'int','required' => false),
+						'offset' => array('type' => 'int', 'required' => false)),
 				"Get list of entities",
 				'GET',
 				false,
