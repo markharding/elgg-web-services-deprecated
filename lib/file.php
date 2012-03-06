8ff53b68476a20d36ccbc9a4f85a195579e4a876
Mark Harding <mark@marksmusiconline.co.uk>
2012-03-06 21:26:39 +0000

    returns fields for files

--- lib/file.php
+++ lib/file.php
@@ -15,15 +15,18 @@
  *
  * @return array $file Array of files uploaded
  */
-function file_get_files($username, $limit = 10, $offset = 0) {	
+function file_get_files($username,  $limit = 10, $offset = 0) {	
 	if(!$username) {
 		$user = get_loggedin_user();
 	} else {
 		$user = get_user_by_username($username);
+		
 	}
 		if (!$user) {
 			throw new InvalidParameterException('registration:usernamenotvalid');
 		}
+		
+		
 		$params = array(
 			'types' => 'object',
 			'subtypes' => 'file',
@@ -31,8 +34,10 @@ function file_get_files($username, $limit = 10, $offset = 0) {
 			'limit' => $limit,
 			'full_view' => FALSE
 		);
+		
+		$latest_file = elgg_get_entities($params);
+		
 	
-	$latest_file = elgg_get_entities($params);
 	if($latest_file) {
 		foreach($latest_file as $single ) {
 			$file[$single->guid]['title'] = $single->title;
@@ -86,12 +91,19 @@ function file_get_files_by_friend($username, $limit = 10, $offset = 0) {
 	if($latest_file) {
 		foreach($latest_file as $single ) {
 			$file[$single->guid]['title'] = $single->title;
-			$file[$single->guid]['owner_guid'] = $single->owner_guid;
+			
+			$owner = get_entity($single->owner_guid);
+			$file[$single->guid]['owner']['guid'] = $owner->guid;
+			$file[$single->guid]['owner']['name'] = $owner->name;
+			$file[$single->guid]['owner']['avatar_url'] = get_entity_icon_url($owner,'small');
+			
 			$file[$single->guid]['container_guid'] = $single->container_guid;
 			$file[$single->guid]['access_id'] = $single->access_id;
 			$file[$single->guid]['time_created'] = $single->time_created;
 			$file[$single->guid]['time_updated'] = $single->time_updated;
 			$file[$single->guid]['last_action'] = $single->last_action;
+			$file[$single->guid]['MIMEType'] = $single->mimetype;
+			$file[$single->guid]['file_icon'] = get_entity_icon_url($single,'small');
 		}
 	}
 	else {
