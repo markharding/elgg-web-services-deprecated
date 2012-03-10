291c9e5e7028aa680b22221cf2db373720c970ba
Mark Harding <mark@marksmusiconline.co.uk>
2012-03-10 20:17:43 +0000

    added $context for mine, all and friends

--- lib/file.php
+++ lib/file.php
@@ -15,18 +15,25 @@
  *
  * @return array $file Array of files uploaded
  */
-function file_get_files($username,  $limit = 10, $offset = 0) {	
+function file_get_files($context,  $limit = 10, $offset = 0, $username) {	
 	if(!$username) {
 		$user = get_loggedin_user();
 	} else {
 		$user = get_user_by_username($username);
-		
-	}
 		if (!$user) {
 			throw new InvalidParameterException('registration:usernamenotvalid');
 		}
+	}
 		
-		
+		if($context == "all"){
+		$params = array(
+			'types' => 'object',
+			'subtypes' => 'file',
+			'limit' => $limit,
+			'full_view' => FALSE
+		);
+		}
+		if($context == "mine"){
 		$params = array(
 			'types' => 'object',
 			'subtypes' => 'file',
@@ -34,9 +41,13 @@ function file_get_files($username,  $limit = 10, $offset = 0) {
 			'limit' => $limit,
 			'full_view' => FALSE
 		);
-		
+		}
 		$latest_file = elgg_get_entities($params);
 		
+		if($context == "friends"){
+		$latest_file = get_user_friends_objects($user->guid, 'file', $limit, $offset);
+		}
+	
 	
 	if($latest_file) {
 		foreach($latest_file as $single ) {
@@ -64,9 +75,11 @@ function file_get_files($username,  $limit = 10, $offset = 0) {
 	
 expose_function('file.get_files',
 				"file_get_files",
-				array('username' => array ('type' => 'string', 'required' => false),
+				array(
+						'context' => array ('type' => 'string', 'required' => false, 'default' => 'all'),
 					  'limit' => array ('type' => 'int', 'required' => false),
 					  'offset' => array ('type' => 'int', 'required' => false),
+					  'username' => array ('type' => 'string', 'required' => false),
 					),
 				"Get file uploaded by all users",
 				'GET',
@@ -91,19 +104,12 @@ function file_get_files_by_friend($username, $limit = 10, $offset = 0) {
 	if($latest_file) {
 		foreach($latest_file as $single ) {
 			$file[$single->guid]['title'] = $single->title;
-			
-			$owner = get_entity($single->owner_guid);
-			$file[$single->guid]['owner']['guid'] = $owner->guid;
-			$file[$single->guid]['owner']['name'] = $owner->name;
-			$file[$single->guid]['owner']['avatar_url'] = get_entity_icon_url($owner,'small');
-			
+			$file[$single->guid]['owner_guid'] = $single->owner_guid;
 			$file[$single->guid]['container_guid'] = $single->container_guid;
 			$file[$single->guid]['access_id'] = $single->access_id;
 			$file[$single->guid]['time_created'] = $single->time_created;
 			$file[$single->guid]['time_updated'] = $single->time_updated;
 			$file[$single->guid]['last_action'] = $single->last_action;
-			$file[$single->guid]['MIMEType'] = $single->mimetype;
-			$file[$single->guid]['file_icon'] = get_entity_icon_url($single,'small');
 		}
 	}
 	else {
