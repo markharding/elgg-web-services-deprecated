18132ada4994de2f2bfe11913dfeabb885195073
Mark Harding <mark@marksmusiconline.co.uk>
2012-03-21 20:30:53 +0000

    Can view a single message when a guid is passed, this also marks the message as read.

--- lib/message.php
+++ lib/message.php
@@ -6,7 +6,50 @@
  * @author Mark Harding / Kramnorth
  *
  */
+/**
+ * Web service to get messages inbox
+ *
+ * @param string $limit  (optional) default 10
+ * @param string $offset (optional) default 0
+ *
+ * @return array $file Array of files uploaded
+ */
+function messages_read($guid) {	
 
+			$single = get_entity($guid);
+			
+			$single->readYet = true;
+	
+			$message[$single->guid]['subject'] = $single->title;
+			
+			$user = get_entity($single->fromId);
+			$message[$single->guid]['user']['guid'] = $user->guid;
+			$message[$single->guid]['user']['name'] = $user->name;
+			$message[$single->guid]['user']['username'] = $user->username;
+			$message[$single->guid]['user']['avatar_url'] = get_entity_icon_url($user,'small');
+			
+			$message[$single->guid]['timestamp'] = $single->time_created;
+			
+			$message[$single->guid]['description'] = $single->description;
+			
+			if($single->readYet){
+			$message[$single->guid]['read'] = "yes";
+			}else{
+			$message[$single->guid]['read'] = "no";
+			}
+	
+			return $message;
+}
+	
+expose_function('messages.read',
+				"messages_read",
+				array(
+					  'guid' => array ('type' => 'int', 'required' => true),
+					),
+				"Read a sigle message",
+				'GET',
+				true,
+				true);
 /**
  * Web service to get messages inbox
  *
