fa3feb03a66a6860a788e0795659b98b7ea31c2a
Mark Harding <mark@marksmusiconline.co.uk>
2012-05-12 17:43:53 +0100

    added first draft of a search with web services.
    Currently only allows for a query to be passed.

--- lib/core.php
+++ lib/core.php
@@ -81,3 +81,76 @@ expose_function('site.river_feed',
 				'GET',
 				false,
 				false);
+				
+/**
+ * Performs a search of the elgg site
+ *
+ * @return array $results search result
+ */
+ 
+function site_search($query){
+	
+	//temp values
+	//these will be added to expose soon
+	$search_type = 'all';
+	$entity_type = ELGG_ENTITIES_ANY_VALUE;
+	$entity_subtype = ELGG_ENTITIES_ANY_VALUE;
+	
+	$params = array(
+					'query' => $query,
+					'search_type' => $search_type,
+					'type' => $entity_type,
+					'subtype' => $entity_type,
+					);
+					
+	$types = get_registered_entity_types();
+	
+	//store the result in here
+	
+
+	foreach ($types as $type => $subtypes) {
+	
+		// pull in default type entities with no subtypes
+		$params['type'] = $type;
+		//$params['subtype'] = ELGG_ENTITIES_NO_VALUE;
+
+		$results = elgg_trigger_plugin_hook('search', $type, $params, array());
+		if ($results === FALSE) {
+			// someone is saying not to display these types in searches.
+			continue;
+		}
+		
+		if($results['count']){
+			foreach($results['entities'] as $single){
+		
+				//search matched critera
+				/*
+				$result['search_matched_title'] = $single->getVolatileData('search_matched_title');
+				$result['search_matched_description'] = $single->getVolatileData('search_matched_description');
+				$result['search_matched_extra'] = $single->getVolatileData('search_matched_extra');
+				*/
+				if($type == 'group' || $type== 'user'){
+				$result['title'] = $single->name;	
+				} else {
+				$result['title'] = $single->title;
+				}
+				$result['guid'] = $single->guid;
+				$result['type'] = $single->type;
+				$result['subtype'] = get_subtype_from_id($single->subtype);
+				
+				$result['avatar_url'] = get_entity_icon_url($single,'small');
+				
+				$return[$type] = $result;
+			}
+		}
+	}
+
+	return $return;
+}
+expose_function('site.search',
+				"site_search",
+				array('query' => array('type' => 'string')),
+				"Perform a search",
+				'GET',
+				false,
+				false);
